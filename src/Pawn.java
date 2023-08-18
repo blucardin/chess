232b5a1d23321d9efd8ea8a31888b68410aecd20
Sam Cardin <blucardin@gmail.com>
2023-08-18 03:26:24 -0400

    Fix pawn class

--- src/Pawn.java
+++ src/Pawn.java
@@ -4,46 +4,41 @@ public class Pawn extends Piece {
 
     public Pawn(boolean isWhite) {
         this.isWhite = isWhite;
-        filePrefix = "p";
+        filePrefix = "p"; // File prefix for image file name
     }
 
     @Override
     public ArrayList<int[]> getPossibleMoves(int x, int y) {
         ArrayList<int[]> possibleMoves = new ArrayList<int[]>();
-        
-        // if the pawn is white, check the square above it to see if it is empty
-        if (isWhite) {
-            if (y > 0 && ChessGame.board.getPieces().get(x).get(y - 1) == null) {
-                possibleMoves.add(new int[]{x, y - 1});
+
+        // Define the pawn's move directions based on its color
+        int moveDirection = isWhite ? -1 : 1;
+
+        // Check if the square directly in front of the pawn is empty
+        int newX = x;
+        int newY = y + moveDirection;
+        if (isValidPosition(newX, newY) && ChessGame.board.getPieces().get(newX).get(newY) == null) {
+            possibleMoves.add(new int[]{newX, newY});
+            
+            // If pawn is on its starting position, check if it can move two squares forward
+            if ((isWhite && y == 6) || (!isWhite && y == 1)) {
+                newY = y + 2 * moveDirection;
+                if (ChessGame.board.getPieces().get(newX).get(newY) == null) {
+                    possibleMoves.add(new int[]{newX, newY});
+                }
             }
-            // if the pawn is on the starting row, check the square two above it to see if it is empty
-            if (y == 6 && ChessGame.board.getPieces().get(x).get(y - 2) == null) {
-                possibleMoves.add(new int[]{x, y - 2});
-            }
-            // if the pawn is on the left side of the ChessGame.board, check the square above and to the right to see if it is occupied by an enemy piece
-            if (x > 0 && y > 0 && ChessGame.board.getPieces().get(x - 1).get(y - 1) != null && !ChessGame.board.getPieces().get(x - 1).get(y - 1).isWhite()) {
-                possibleMoves.add(new int[]{x - 1, y - 1});
-            }
-            // if the pawn is on the right side of the ChessGame.board, check the square above and to the left to see if it is occupied by an enemy piece
-            if (x < 7 && y > 0 && ChessGame.board.getPieces().get(x + 1).get(y - 1) != null && !ChessGame.board.getPieces().get(x + 1).get(y - 1).isWhite()) {
-                possibleMoves.add(new int[]{x + 1, y - 1});
-            }
-        } else {
-            // if the pawn is black, check the square below it to see if it is empty
-            if (y < 7 && ChessGame.board.getPieces().get(x).get(y + 1) == null) {
-                possibleMoves.add(new int[]{x, y + 1});
-            }
-            // if the pawn is on the starting row, check the square two below it to see if it is empty
-            if (y == 1 && ChessGame.board.getPieces().get(x).get(y + 2) == null) {
-                possibleMoves.add(new int[]{x, y + 2});
-            }
-            // if the pawn is on the left side of the ChessGame.board, check the square below and to the right to see if it is occupied by an enemy piece
-            if (x > 0 && y < 7 && ChessGame.board.getPieces().get(x - 1).get(y + 1) != null && ChessGame.board.getPieces().get(x - 1).get(y + 1).isWhite()) {
-                possibleMoves.add(new int[]{x - 1, y + 1});
-            }
-            // if the pawn is on the right side of the ChessGame.board, check the square below and to the left to see if it is occupied by an enemy piece
-            if (x < 7 && y < 7 && ChessGame.board.getPieces().get(x + 1).get(y + 1) != null && ChessGame.board.getPieces().get(x + 1).get(y + 1).isWhite()) {
-                possibleMoves.add(new int[]{x + 1, y + 1});
+        }
+
+        // Check diagonal captures
+        int[] captureOffsets = { -1, 1 };
+        for (int offset : captureOffsets) {
+            newX = x + offset;
+            newY = y + moveDirection;
+            if (isValidPosition(newX, newY)) {
+                Piece targetPiece = ChessGame.board.getPieces().get(newX).get(newY);
+                if (targetPiece != null && targetPiece.isWhite() != isWhite) {
+                    possibleMoves.add(new int[]{newX, newY});
+                }
             }
         }
 
@@ -54,6 +49,4 @@ public class Pawn extends Piece {
     public String toString() {
         return "Pawn [isWhite=" + isWhite + "]";
     }
-
-
 }
